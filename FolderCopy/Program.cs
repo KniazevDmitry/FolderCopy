f61f830f28c1c3e4559f003d61fb95cc04cccaff
Dima Kniazev <xojlmc@ya.ru>
2023-05-29 20:59:10 +0400

    Added md5 check

--- FolderCopy/Program.cs
+++ FolderCopy/Program.cs
@@ -1,33 +1,56 @@
-﻿namespace FolderCopy;
+﻿using System.Security.Cryptography;
+
+namespace FolderCopy;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("To set up a copy job use the following format:");
-        Console.WriteLine("[Source folder path] [Target folder path]");
-        
+        Console.WriteLine("[Source folder path] [Target folder path]");      
 
+        string sourceFolder = "/Users/vmdk/Downloads/Source";
+        string targetFolder = "/Users/vmdk/Downloads/Target";
 
-        string sourceFolder = @"/Users/vmdk/Downloads/Source";
-        string targetFolder = @"/Users/vmdk/Downloads/Target";
+        //create target folder if it doesn't exist
+        if (!Directory.Exists(targetFolder))
+        {
+            Directory.CreateDirectory(targetFolder);
+        }
 
         SyncFolder(sourceFolder, targetFolder);
-        Console.WriteLine("Copy complete");
+        Console.WriteLine("Copy job complete");
 
     }
 
     static void SyncFolder(string SourceFolder, string TargetFolder)
-    {
-
-        if (!Directory.Exists(TargetFolder))
-        {
-            Directory.CreateDirectory(TargetFolder);
-        }
-
+    {             
         foreach (string sourceFilePath in Directory.GetFiles(SourceFolder))
         {
             string sourceFileName = Path.GetFileName(sourceFilePath);
             string targetPath = Path.Combine(TargetFolder, sourceFileName);
+
+            //calculate md5 of both files if it already exists in target folder
+            if (File.Exists(targetPath))
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var readSourceFile = File.OpenRead(sourceFilePath))
+                    {
+                        using (var readTargetFile = File.OpenRead(targetPath))
+                        {
+                            var hashSourceFile = md5.ComputeHash(readSourceFile);
+                            var hashTargetFile = md5.ComputeHash(readTargetFile);
+
+                            if (hashSourceFile == hashTargetFile)
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                    
+
+                }
+            }
 
             File.Copy(sourceFilePath, targetPath, true);
         }
